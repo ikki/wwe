9dbc793fe7be8d9634654196299f4decbb48c1de
ikki <81405612+ikki@users.noreply.github.com>
2021-04-19 12:15:14 +0530

    Create powered.cs

---
+++ powered.cs
@@ -0,0 +1,36 @@
+.center {
+   text-align: center;
+
+
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #000;
+}
+
+
+p {
+  position: relative;
+  font-family: Dishonorable Mention;
+  text-transform: uppercase;
+  font-size:1em;
+  letter-spacing: 1px;
+  overflow: hidden;
+  background: linear-gradient(80deg, #17C0DD, #FFF4F2, #0F7E91);
+  background-repeat: loop;
+  background-size: 70%;
+  animation: animate 5s linear infinite;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: rgba(255, 255, 255,0);
+}
+
+@keyframes animate {
+  0% {
+    background-position: -500%;
+  }
+  100% {
+    background-position: 500%;
+  }
+}
